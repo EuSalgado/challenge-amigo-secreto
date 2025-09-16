5a75b2d31594e6b6e1060b54718086cce6e75fa3
EuSalgado <eustorgiosalgado209@gmail.com>
2025-09-16 17:02:35 -0600

    Se implemento la función agregaraAmigo()

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let amigo = '';
+
+function agregarAmigo() {
+  const inputElement = document.getElementById('amigo');
+  const nombreAmigo = inputElement.value;
+
+  if (nombreAmigo.trim() === '') {
+    alert('Por favor, inserte un nombre');
+  } else {
+    amigos.push(amigo);
+    inputElement.value = '';
+  }
+}
